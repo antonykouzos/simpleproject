f3d895371e873bc92ba6c4e2bc0826e8fa5a077f
antonykouzos <antonykouzos@gmail.com>
2014-08-19 02:15:53 +0300

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+
+function TodoCtrl($scope) {
+  $scope.todos = [];
+  $scope.markAll = false;
+    
+  $scope.addTodo = function() {
+      if(event.keyCode == 13 && $scope.todoText){
+          $scope.todos.push({text:$scope.todoText, done:false});
+          $scope.todoText = '';
+      }
+  };
+  $scope.isTodo = function(){
+      return $scope.todos.length > 0;  
+  }
+  $scope.toggleEditMode = function(){
+      $(event.target).closest('li').toggleClass('editing');
+  };
+  $scope.editOnEnter = function(todo){
+      if(event.keyCode == 13 && todo.text){
+          $scope.toggleEditMode();
+      }
+  };
+    
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  $scope.hasDone = function() {
+      return ($scope.todos.length != $scope.remaining());
+  }    
+    
+  $scope.itemText = function() {
+      return ($scope.todos.length - $scope.remaining() > 1) ? "items" : "item";     
+  };
+      
+  $scope.toggleMarkAll = function() {
+      angular.forEach($scope.todos, function(todo) {
+        todo.done =$scope.markAll;
+      });
+  };
+  
+  $scope.clear = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+  };
+    
+}
+
